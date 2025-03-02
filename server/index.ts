bb516a701ce1bc60b48b9d23bfd162a5499603f4
pankajkandpal <kandpalpankaj34@gmail.com>
2025-03-02 17:58:27 +0530

    fix index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,46 +1,25 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { corsConfig } from "./config/corsConfig";
 import { Request, Response } from "express";
 import { userRoutes } from "./routes/userRoutes";
 import connectDB from "./config/db";
 dotenv.config();
 
 const app = express();
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "https://react-clicker.vercel.app",
-  ];
-  const origin = req.headers.origin as string;
 
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://react-clicker.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Accept", "Origin", "X-Requested-With"],
+  })
+);
 
-app.options("*", (req, res) => {
-  const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "https://react-clicker.vercel.app",
-  ];
-  const origin = req.headers.origin as string;
-
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.status(204).send();
-});
-
-app.use(cors(corsConfig));
 app.use(express.json());
 
 app.get("/", (_req: Request, res: Response) => {
