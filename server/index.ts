d1e5c82c61bb0e6fded2a6b22bc8d0207675eacf
pankajkandpal <kandpalpankaj34@gmail.com>
2025-03-02 15:40:31 +0530

    add vercel.json in server

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { corsConfig } from "./config/corsConfig";
+import { Request, Response } from "express";
 import { userRoutes } from "./routes/userRoutes";
 import connectDB from "./config/db";
 
@@ -12,14 +13,14 @@ const app = express();
 app.use(cors(corsConfig));
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (_req: Request, res: Response) => {
   res.send("Backend is running!");
 });
 
 app.use("/api", userRoutes);
 
+const PORT = process.env.PORT || 3000;
 connectDB().then(() => {
-  const PORT = process.env.PORT || 8000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
