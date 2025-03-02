5b4bd13ef25359bd3e153d5d368bdb88c28c7a54
pankajkandpal <kandpalpankaj34@gmail.com>
2025-03-02 16:09:26 +0530

    update

--- server/config/corsConfig.ts
+++ server/config/corsConfig.ts
@@ -1,8 +1,7 @@
 export const corsConfig = {
   origin: ["http://localhost:5173", "https://react-clicker.vercel.app"],
-
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type"],
   credentials: true,
   preflightContinue: false,
   optionsSuccessStatus: 204,
