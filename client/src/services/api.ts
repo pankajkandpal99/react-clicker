7a0c1822e6dc0c7e8ccd78b829c9dbf53d30de8b
pankajkandpal <kandpalpankaj34@gmail.com>
2025-03-02 16:55:19 +0530

    update

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_MY_BASE_URL;
+const BASE_URL = import.meta.env.BASE_URL || "https://react-clicker.vercel.app";
 
 const api = axios.create({
   baseURL: `${BASE_URL}/api`,
