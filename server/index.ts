35ca34e06c8ed8125d6abe0f2a088238a9b06d19
pankajkandpal <kandpalpankaj34@gmail.com>
2025-03-02 16:29:07 +0530

    update cors

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,21 @@ dotenv.config();
 const app = express();
 
 app.use(cors(corsConfig));
-app.options("*", cors(corsConfig));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://react-clicker.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "https://react-clicker.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.status(204).send();
+});
+
 app.use(express.json());
 
 app.get("/", (_req: Request, res: Response) => {
